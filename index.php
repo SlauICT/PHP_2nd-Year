8cd4789c68ef43e35c7680e33956f38e7c22f239
Kevin Janvier <kevin.janvier5@gmail.com>
2014-10-21 03:14:05 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,23 +2,50 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Untitled Document</title>
+<title>St.lawrence University | Php Class</title>
 </head>
 
 <body>
 <form id="form1" name="form1" method="post" action="connect.php">
-  <p>FISTNAME : 
-    <label for="Fname"></label>
-  <input type="text" name="Fname" id="Fname" />
-  </p>
-  <p>SECONDNAME
-    <label for="Lname"></label>
-    <input type="text" name="Lname" id="Lname" />
-  </p>
+  <table width="75%" border="0" align="center">
+      <tr>
+        <td align="center"><h1>ST.LAWRENCE UNIVERSITY PHP CLASS<br /> INSERT AND VIEW DATA</h1></td>
+      </tr>
+    </table>
+  <table width="30%" border="0" align="center">
+    <tr>
+      <td width="40%">FISTNAME :</td>
+      <td colspan="2"><input type="text" name="Fname" id="Fname" required="required" /></td>
+    </tr>
+    <tr>
+      <td>SECONDNAME:
+        <label for="Lname"></label></td>
+      <td colspan="2"><input type="text" name="Lname" id="Lname" required="required" /></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td width="19%"><input type="submit" name="button" id="button" value="Send" /></td>
+      <td width="41%"><input type="reset" name="button2" id="button2" value="Reset" /></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td colspan="2">&nbsp;</td>
+    </tr>
+    <tr>
+      <td>Lecturer:</td>
+      <td colspan="2">John Bosco Lubega</td>
+    </tr>
+  </table>
+  View Data in the Database <a href="view.php">Here</a>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
   <p>
-    <input type="submit" name="button" id="button" value="Send" />
-    <input type="reset" name="button2" id="button2" value="Reset" />
+    <label for="Fname"></label>
   </p>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
 </form>
 </body>
 </html>
